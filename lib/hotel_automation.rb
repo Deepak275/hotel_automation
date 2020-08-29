a39205e5bc63166c918ecd32e4ae83ab274ccabf
Deepak Kumar <deepakkumar@Deepaks-MacBook-Pro.local>
2020-08-29 18:04:28 +0530

    intial setup

---
+++ lib/hotel_automation.rb
@@ -0,0 +1,6 @@
+require "hotel_automation/version"
+
+module HotelAutomation
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/hotel_automation_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe HotelAutomation do
+  it "has a version number" do
+    expect(HotelAutomation::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
